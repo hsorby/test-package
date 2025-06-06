d3a006a2d9f89cc73f51fab7db61a5adfd650902
Hugh Sorby <h.sorby@auckland.ac.nz>
2025-06-06 14:43:32 +1200

    Remove normalized path.

--- src/import_package.py
+++ src/import_package.py
@@ -1,7 +1,7 @@
 import importlib.metadata
 
 for i in importlib.metadata.distributions():
-    print(i.metadata['Name'], i._normalized_name)
+    print(i.metadata['Name'])
 
 import cmlibs.maths
 import cmlibs.utils
